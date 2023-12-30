0ac4edd32896f2c5b6f73277228338da685cbb4f
ervanervan <ervanervan040@gmail.com>
2023-12-30 19:17:37 +0700

    Update movie list

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MovieCard from "./MovieCard";
 import GlobalApi from "../services/GlobalApi";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 
 export default function MovieList({ genreId }) {
   const [movieList, setMovieList] = useState([]);
+  const elementRef = useRef(null);
 
   useEffect(() => {
     getMovieByGenreId();
@@ -16,11 +18,33 @@ export default function MovieList({ genreId }) {
     });
   };
 
+  const sliderRight = (element) => {
+    element.scrollLeft += 500;
+  };
+  const sliderLeft = (element) => {
+    element.scrollLeft -= 500;
+  };
+
   return (
-    <div className="flex overflow-x-auto gap-5 scrollbar-hide px-5 md:px-10">
-      {movieList.map((item, index) => (
-        <MovieCard movie={item} key={index} />
-      ))}
+    <div className="relative">
+      <ChevronLeftIcon
+        className="absolute w-8 h-8 text-gray-200 cursor-pointer left-10 top-1/2 -translate-y-1/2 hidden md:block z-30"
+        onClick={() => sliderLeft(elementRef.current)}
+      />
+
+      <div
+        ref={elementRef}
+        className="flex overflow-x-auto gap-5 scrollbar-hide px-5 md:px-10"
+      >
+        {movieList.map((item, index) => (
+          <MovieCard movie={item} key={index} />
+        ))}
+      </div>
+
+      <ChevronRightIcon
+        className="absolute w-8 h-8 text-gray-200 cursor-pointer right-10 top-1/2 -translate-y-1/2 hidden md:block z-30"
+        onClick={() => sliderRight(elementRef.current)}
+      />
     </div>
   );
 }
