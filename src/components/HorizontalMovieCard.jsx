83a2d9fa46d96cf72728dbd1865cda3950e4d94b
ervanervan <ervanervan040@gmail.com>
2023-12-30 19:42:00 +0700

    Update horizontal movie card

--- src/components/HorizontalMovieCard.jsx
+++ src/components/HorizontalMovieCard.jsx
@@ -8,7 +8,7 @@ export default function HorizontalMovieCard({ movie }) {
         src={IMAGE_BASE_URL + movie.backdrop_path}
         alt=""
         width={300}
-        className="w-full cursor-pointer max-w-32 md:max-w-56 rounded-md object-cover border-2 border-gray-900 hover:border-2 hover:border-gray-300 hover:scale-[102%] transition-all duration-300 ease-in-out"
+        className="cursor-pointer max-w-32 md:max-w-56 rounded-md object-cover border-2 border-gray-900 hover:border-2 hover:border-gray-300 hover:scale-[102%] transition-all duration-300 ease-in-out"
       />
     </div>
   );
